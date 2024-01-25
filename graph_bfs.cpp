dcc31ef511ab92b0dafadf70de1c48d30b60999e
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-01-25 05:47:59 +0530

    Create graph_bfs.cpp

---
+++ graph_bfs.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#include<map>
+#include <list>
+#include<queue>
+template<typename T>
+
+class graph{
+   public:
+   map<T,list<T>> l;
+   void addedge(int x,int y){
+       l[x].push_back(y);
+       l[y].push_back(x);
+   }
+   void bfs(T src){
+       map<T,int> visited;
+       queue<T> q;
+       q.push(src);
+       visited[src]=true;
+       cout<<src<<' ';
+       while(!q.empty()){
+           T node=q.front();
+           q.pop();
+           for(int d:l[node]){
+               if(!visited[d]){
+                   cout<<d<<' ';
+                   q.push(d);
+                   visited[d]=true;
+               }
+           }
+       }
+   }
+    
+};
+int main(){
+    graph<int> g;
+    g.addedge(1,2);
+    g.addedge(2,3);
+    g.addedge(1,3);
+    g.addedge(4,1);
+    g.bfs(1);
+}
