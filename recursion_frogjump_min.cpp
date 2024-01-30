f07af69736286be1a6d92e94d01b6dad1adc4232
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-01-30 05:53:55 +0530

    Create recursion_frogjump_min.cpp

---
+++ recursion_frogjump_min.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//recursion->take all possible steps
+int frog_jump(int n,int a[]){
+    if(n==0){
+        return 0;
+    }
+    int left=frog_jump(n-1,a)+abs(a[n]-a[n-1]);
+    int right=0;
+    if(n>1){
+        right=frog_jump(n-2,a)+abs(a[n]-a[n-2]);
+    }
+    return min(left,right);
+}
+int main(){
+    int a[]={10,20,30,10};
+    int n=frog_jump(4,a);
+    cout<<"The minimum no. of distance covered by the from during jump:"<<n<<endl;
+}
