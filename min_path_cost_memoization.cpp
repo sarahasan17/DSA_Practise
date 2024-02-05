f8e70546a20356c973681c47ae69414890b1e46e
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-02-05 06:01:42 +0530

    Create min_path_cost_memoization.cpp

---
+++ min_path_cost_memoization.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int minpath(int a[3][3],int i,int j,int dp[3][3]){
+    if(i==0 && j==0){
+        return a[0][0];
+    }
+    if(i<0 || j<0){
+        return INT_MAX;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    else{
+        int left=0;
+        int right=0;
+        if(i>0){
+            left=minpath(a,i-1,j,dp)+a[i][j];
+        }
+        if(j>0){
+            right=minpath(a,i,j-1,dp)+a[i][j];
+        }
+        return dp[i][j]=min(left,right);
+    }
+}
+int main()
+{
+    int dp[3][3]={{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
+    int a[3][3]={{0,1,2},{3,4,5},{6,7,8}};
+    cout<<minpath(a,2,2,dp);
+}
