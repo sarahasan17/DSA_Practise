828e1f95ea42bf16aeb8f41434fae8f9aae995a6
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-02-20 06:43:06 +0530

    Create dp_equal_subset_sum.cpp

---
+++ dp_equal_subset_sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n,vector<bool>(k+1,0));
+    for(int i=0;i<n;i++){
+        dp[i][0]=true;
+    }
+    dp[0][arr[0]]=true;
+    for(int i=1;i<n;i++){
+        for(int target=1;target<=k;target++){
+            bool nottake=dp[i-1][target];
+            bool take=false;
+            if (target >=arr[i]) {
+              take = dp[i - 1][ target - arr[i]];
+            }
+            dp[i][target]=(take|nottake);
+        }
+    }
+    return dp[n-1][k];
+}
+bool canPartition(vector<int> &arr, int n)
+{
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+	}
+	if(sum%2) return false;
+	int target=sum/2;
+	return subsetSumToK(n,target,arr);
+}
