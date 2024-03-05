f2fa8643a9972f247cc4d61dd923ba5c4a2ce2a4
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-03-05 07:16:16 +0530

    Update dp_frogjump_tabular.cpp

--- dp_frogjump_tabular.cpp
+++ dp_frogjump_tabular.cpp
@@ -15,3 +15,20 @@ int main(){
     }
     cout<<"The minimum no. of distance covered by the from during jump:"<<dp[3]<<endl;
 }
+/**FOR CODING NINJA
+#include <bits/stdc++.h> 
+
+int frogJump(int n, vector<int> &heights)
+{
+    int dp[n];
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        int l1=dp[i-1]+abs(heights[i]-heights[i-1]);
+        int l2=1000000;
+        if(i>1){
+            l2=dp[i-2]+abs(heights[i]-heights[i-2]);
+        }
+        dp[i]=min(l1,l2);
+    }
+    return dp[n-1];
+}**/
