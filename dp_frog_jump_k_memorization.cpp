3d54e21ab78de95765fb820bb27a00879a09bff8
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-03-05 11:41:01 +0530

    Update dp_frog_jump_k_memorization.cpp

--- dp_frog_jump_k_memorization.cpp
+++ dp_frog_jump_k_memorization.cpp
@@ -1,36 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
 using namespace std;
-//top-down approach_memoriation
-int frog_jump(int n,int a[],int dp[],int k){
+int frog_k(int n,int a[],vector<int> dp,int k){
     if(n==0){
         return 0;
     }
     if(dp[n]!=-1){
         return dp[n];
     }
-    int minu=INT_MAX;
-    for(int j=1;j<=k;j++){
-        if(n-j>=0){
-           int left=frog_jump(n-j,a,dp,k)+abs(a[n]-a[n-j]);
-           minu=min(minu,left);
+    int minu=100000;
+    for(int i=1;i<=k;i++){
+        if(n-i>=0){
+            int val=frog_k(n-k,a,dp,k)+abs(a[n]-a[n-i]);
+            minu=min(val,minu);
         }
     }
     return dp[n]=minu;
 }
-int frogjump(int n,int a[],int k){
-    int dp[]={-1,-1,-1,-1};
-    return frog_jump(n-1,a,dp,k);
-}
-int main(){
+int main()
+{
     int a[]={10,20,30,10};
-    /*for(int i=1;i<4;i++){
-        int left=previ+abs(a[i]-a[i-1]);
-        int right=10000000;
-        if(i>1){
-            right=prev2i+abs(a[i]-a[i-2]);
-        }
-        prev2i=previ;
-        previ=min(left,right);
-    }*/
-    cout<<"The minimum no. of distance covered by the from during jump:"<<frogjump(4,a,2)<<endl;
+    vector<int> dp(4,-1);
+    cout<<"The minimum no. of distance covered by the from during jump:"<<frog_k(3,a,dp,2)<<endl;
 }
