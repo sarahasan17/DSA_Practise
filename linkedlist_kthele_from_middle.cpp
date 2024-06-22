9b13059c91dfde6aba7b3c0095bb6101ec568b62
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-06-22 08:30:24 +0530

    Update linkedlist_kthele_from_middle.cpp

--- linkedlist_kthele_from_middle.cpp
+++ linkedlist_kthele_from_middle.cpp
@@ -1,69 +1,57 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 class node{
     public:
     int data;
-    node* next;
-    node(){
-        data=0;
-        next=NULL;
-    }
-    node(int a){
-        data=a;
+    node *next;
+    node(int val){
+        data=val;
         next=NULL;
     }
 };
-
 class LinkedList{
-   public:
-   node *head;
-   void insert(int a){
-       if(head==NULL){
-           node *temp=new node(a);
-           head=temp;
-           return;
-       }
-       node *curr=head;
-       while(curr->next!=NULL){
-           curr=curr->next;
-       }
-       node *temp=new node(a);
-       curr->next=temp;
-   }
- 
-   int kthnode_from_middle(int a){
-       node *curr=head;
-       int count=0;
-       while(curr!=NULL){
-           count+=1;
-           curr=curr->next;
-       }
-       node *temp=head;
-       int i=0;
-       while(i!=int(count/2)-a){
-           i++;
-           temp=temp->next;
-       }
-       return temp->data;
-       
-   }
-    void display(){
-       node *curr=head;
-       while(curr!=NULL){
-           cout<<curr->data<<"-->";
-           curr=curr->next;
-       }
-   }
+    public:
+    node *head;
+    LinkedList(){
+        head=NULL;
+    }
+    void insert(int a){
+        node *temp=new node(a);
+        if(head==NULL){
+            head=temp;
+        }
+        else{
+            node *t=head;
+            while(t->next!=NULL){
+                t=t->next;
+            }
+            t->next=temp;
+        }
+    }
+    void kth_midd(int b){
+        node *t=head;
+        int c=0;
+        while(t!=NULL){
+            t=t->next;
+            c++;
+        }
+        int g=0;
+        int k=c/2;
+        t=head;
+        while(g!=k-b){
+            t=t->next;
+            g++;
+        }
+        cout<<t->data;
+    }
 };
 int main(){
-    LinkedList list;
-    list.insert(1);
-    list.insert(2);
-    list.insert(3);
-    list.insert(4);
-    list.insert(5);
-    list.insert(6);
-    cout<<list.kthnode_from_middle(2)<<endl;
+    LinkedList l=LinkedList();
+    l.insert(1);
+    l.insert(2);
+    l.insert(3);
+    l.insert(4);
+    l.insert(5);
+    l.kth_midd(2);
 }
