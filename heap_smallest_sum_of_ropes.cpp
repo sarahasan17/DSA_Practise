dd403a24412f6442c4dea19131163fbf16a12b25
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-06-30 17:02:15 +0530

    Update heap_smallest_sum_of_ropes.cpp

--- heap_smallest_sum_of_ropes.cpp
+++ heap_smallest_sum_of_ropes.cpp
@@ -26,3 +26,26 @@ int main(){
     }
     cout<<val<<endl;
 }
+// METHOD 2
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// int main(){
+//     vector<int> a={4,3,2,6};
+//     priority_queue<int,vector<int>,greater<int>> q(a.begin(),a.end());
+//     int val=0;
+//     while(!q.empty()){
+//         int n1=q.top();
+//         q.pop();
+//         int n2=q.top();
+//         q.pop();
+//         int sum=n1+n2;
+//         val+=sum;
+//         if(q.size()>=1){
+//             q.push(sum);
+//         }
+//     }
+//     cout<<val;
+// }
+
