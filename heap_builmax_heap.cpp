2c5ee647c61b44a27969c714f6b5b153ba060c9c
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-06-30 10:20:46 +0530

    Update heap_builmax_heap.cpp

--- heap_builmax_heap.cpp
+++ heap_builmax_heap.cpp
@@ -1,40 +1,30 @@
-#include<iostream>
-#include <conio.h>
+#include <iostream>
 using namespace std;
 
-void maxheapify(int a[],int k,int n){
-    int j,temp;
-    temp=a[k];
-    j=2*k;
+void maxheapify(int a[],int i,int n){
+    int temp=a[i];
+    int j=2*i+1;
     while(j<n){
-        if(j<n && a[j]<a[j+1]){
+        if(j+1<n && a[j]<a[j+1]){
             j=j+1;
         }
         if(temp>a[j]){
             break;
         }
-        else if(temp<=a[j]){
-            a[j/2]=a[j];
-            j=2*j;
-        }
+        a[(j-1)/2]=a[j];
+        j=2*j+1;
     }
-    a[j/2]=temp;
+    a[(j-1)/2]=temp;
     return;
 }
-
-void buildmaxheap(int a[],int n){
-    for(int i=n/2;i>=0;i--){
+void maxheap(int a[],int n){
+    for(int i=(n/2);i>=0;i--){
         maxheapify(a,i,n);
     }
 }
-
 int main(){
-    int a[7];
-    cout<<"Enter the elements of the array"<<endl;
-    for(int i=0;i<7;i++){
-        cin>>a[i];
-    }
-    buildmaxheap(a,7);
+    int a[]={5,3,8,2,6,9,1};
+    maxheap(a,7);
     for(int i=0;i<7;i++){
         cout<<a[i]<<" ";
     }
